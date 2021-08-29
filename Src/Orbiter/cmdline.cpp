60318edc3f03e22d24c302b1cc96ab993a2044f8
DarkWanderer <elquentaro@gmail.com>
2021-08-29 10:34:15 +0300

    WIP

--- Src/Orbiter/cmdline.cpp
+++ Src/Orbiter/cmdline.cpp
@@ -3,9 +3,81 @@
 
 #include <iostream>
 #include <set>
+#include <string_view>
+
 #include "cmdline.h"
 #include "Orbiter.h"
 #include "Launchpad.h"
+
+LPSTR* CommandLineToArgvA(LPSTR lpCmdLine, INT* pNumArgs)
+{
+	int retval;
+	retval = MultiByteToWideChar(CP_ACP, MB_ERR_INVALID_CHARS, lpCmdLine, -1, NULL, 0);
+	if (!SUCCEEDED(retval))
+		return NULL;
+
+	LPWSTR lpWideCharStr = (LPWSTR)malloc(retval * sizeof(WCHAR));
+	if (lpWideCharStr == NULL)
+		return NULL;
+
+	retval = MultiByteToWideChar(CP_ACP, MB_ERR_INVALID_CHARS, lpCmdLine, -1, lpWideCharStr, retval);
+	if (!SUCCEEDED(retval))
+	{
+		free(lpWideCharStr);
+		return NULL;
+	}
+
+	int numArgs;
+	LPWSTR* args;
+	args = CommandLineToArgvW(lpWideCharStr, &numArgs);
+	free(lpWideCharStr);
+	if (args == NULL)
+		return NULL;
+
+	int storage = numArgs * sizeof(LPSTR);
+	for (int i = 0; i < numArgs; ++i)
+	{
+		BOOL lpUsedDefaultChar = FALSE;
+		retval = WideCharToMultiByte(CP_ACP, 0, args[i], -1, NULL, 0, NULL, &lpUsedDefaultChar);
+		if (!SUCCEEDED(retval))
+		{
+			LocalFree(args);
+			return NULL;
+		}
+
+		storage += retval;
+	}
+
+	LPSTR* result = (LPSTR*)LocalAlloc(LMEM_FIXED, storage);
+	if (result == NULL)
+	{
+		LocalFree(args);
+		return NULL;
+	}
+
+	int bufLen = storage - numArgs * sizeof(LPSTR);
+	LPSTR buffer = ((LPSTR)result) + numArgs * sizeof(LPSTR);
+	for (int i = 0; i < numArgs; ++i)
+	{
+		BOOL lpUsedDefaultChar = FALSE;
+		retval = WideCharToMultiByte(CP_ACP, 0, args[i], -1, buffer, bufLen, NULL, &lpUsedDefaultChar);
+		if (!SUCCEEDED(retval))
+		{
+			LocalFree(result);
+			LocalFree(args);
+			return NULL;
+		}
+
+		result[i] = buffer;
+		buffer += retval;
+		bufLen -= retval;
+	}
+
+	LocalFree(args);
+
+	*pNumArgs = numArgs;
+	return result;
+}
 
 CommandLine::CommandLine(const PSTR cmdLine)
 {
@@ -32,14 +104,53 @@ void CommandLine::ParseCmdLine(const PSTR cmdLine)
 {
 	PSTR pc = cmdLine;
 	bool groupKey = false;
+	int nArgs;
 
-	while (*pc) {
-		Option option;
-		option.key = 0;
-		if (ParseNextOption(pc, groupKey, option)) {
-			optionList.push_back(option);
+	auto args = CommandLineToArgvA(cmdLine, &nArgs);
+
+	Option option;
+	for (int i = 0; i < nArgs; i++) {
+		auto arg = args[i];
+		auto len = strlen(arg);
+		if (len < 2 || arg[0] != '-')
+			continue;
+		if (arg[1] == '-') // "Long" parameter "--scenario"
+		{
+			auto eqPos = strstr(arg + 2, "=");
+			if (eqPos) // --scenario=MyScenario
+			{
+				*eqPos = '\0'; // replace '=' with null byte
+				option.strKey = arg + 2;
+				option.strVal = eqPos + 1;
+			}
+			else // --scenario MyScenario
+			{
+				option.strKey = arg;
+				i++;
+				if (i < nArgs) {
+					option.strVal = args[i];
+				}
+				else {
+					option.strVal = "";
+				}
+			}
 		}
+		else // "Short" parameter form - "-s"
+		{
+			if (i + 1 == nArgs || args[i + 1][0] == '-')
+			{
+				option.strVal = "";
+			}
+			else
+			{
+				i++;
+				option.strVal = args[i];
+			}
+		}
+		optionList.push_back(option);
+		LocalFree(args[i]);
 	}
+	LocalFree(args);
 }
 
 bool CommandLine::ParseNextOption(PSTR& cmdLine, bool& groupKey, Option& option)
